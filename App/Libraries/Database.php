1a76798227946e598e97a8b0517ee8f25782aaa8
Paulo Lacerda da Silva <3304139@ifro.local>
2024-10-01 10:36:52 -0400

    database

--- App/Libraries/Database.php
+++ App/Libraries/Database.php
@@ -1,7 +1,29 @@
 <?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=blogAula', 'root', '');
-} catch (PDOException $e) {
-    // tentar reconectar após algum intervalo, por exemplo
-    echo "Error".$e;
+class Database{
+    private $host = "localhost";
+    private $usuario = "root";
+    private $senha = "";
+    private $banco = "blogaula";
+    private $porta = "3306"; //verificar a porta do seu banco
+    private $dbh;
+
+    public function __construct(){
+        //fonte de dados ou DNS  que contém as informações para conectar ao banco de dados.
+        $dns = 'mysql:host='.$this->host.';port='.$this->porta.';dbname='.$this->banco;
+        
+        $opcoes = [
+             //armanezar em cache a conexão para ser reutilizada, evitando sobrecarga de uma nova conexão. 
+            PDO::ATTR_PERSISTENT => true,
+            //lança um PDOException se ocorrer um erro
+            PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION
+        ];
+        try{
+            //cria a instacia do PDO
+            $this->dbh = new PDO($dns, $this->usuario, $this->senha, $opcoes)
+        }catch(PDOException $error){
+          print "Error!";$error->getMessage()."<br/>";
+          die();
+          //fala
+        }//fim do catch
+    }//fim do método construtor
 }
