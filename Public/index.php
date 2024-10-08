b9314c108a5dbcde858356eabdb8d46ef77cddc5
paulolacerdasilva <plslacerda@gmail.com>
2024-10-08 10:33:13 -0400

    testando o banco

--- Public/index.php
+++ Public/index.php
@@ -3,6 +3,66 @@ include "./../App/configuracao.php";
 include "./../App/Libraries/Rota.php";
 include "./../App/Libraries/Controller.php";
 include "./../App/Libraries/Database.php";
+
+$db = new Database;
+//EXIBE VÁRIOS RESULTADOS
+$db->query("SELECT * FROM posts");
+foreach($db->resultados() as $post){
+    echo $post->titulo. ' | '.$post->texto.'<br>';
+}
+/*
+//EXIBE APENAS UM RESULTADO
+$db->query("SELECT * FROM posts ORDER BY id DESC");
+$db->resultado();
+echo $db->resultado()->titulo;
+*/
+/*
+//deletando linha do banco
+$id = 1;
+$db->query("DELETE FROM posts WHERE id = :id");
+$db->bind(":id",$id);
+$db->executa();
+echo '<hr>Total atualizado: '.$db->totalResultados();
+
+*/
+/*
+//atualizando banco
+date_default_timezone_set('America/Cuiaba');
+$id = 3;
+$usuarioId = 8;
+$titulo = "Algoritmos";
+$texto = "Algoritmos é muito legal";
+$criadoEm = date('Y-m-d H:i:s');
+
+$db->query("UPDATE posts SET usuario_id = :usuario_id, titulo = :titulo, texto = :texto, criado_em=:criadoEm WHERE id = :id");
+
+$db->bind(":id",$id);
+$db->bind(":usuario_id",$usuarioId);
+$db->bind(":titulo",$titulo);
+$db->bind(":texto",$texto);
+$db->bind(":criadoEm",$criadoEm);
+
+$db->executa();
+echo '<hr>Total Resultados: '.$db->totalResultados();
+*/
+
+/*
+//inserindo dados no banco
+$usuario_id = 8;
+$titulo = 'Programação';
+$texto = 'Programação web';
+
+$db->query("INSERT INTO posts (usuario_id, titulo, texto) VALUES (:usuario_id, :titulo, :texto)");
+$db->bind(":usuario_id",$usuario_id);
+$db->bind(":titulo",$titulo);
+$db->bind(":texto",$texto);
+
+$db->executa();
+
+echo '<hr>Total Resultados: '.$db->totalResultados();
+echo '<hr>Ultimo ID inserido'.$db->ultimoIdInserido();
+*/
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
